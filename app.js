df45cdcc325aa330e47f165d86449225f990093d
prasannanjaneyulubandaru-gif <prasannanjaneyulu.bandaru@gmail.com>
2025-12-11 18:41:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -275,8 +275,6 @@ function setupEventListeners() {
     
     // Strike Selection Page
      setupStrategiesListeners();
-    // Option spreads page
-     setupOptionsStreadsListeners();
 }
 
 function handleCredentialsSubmit(e) {
